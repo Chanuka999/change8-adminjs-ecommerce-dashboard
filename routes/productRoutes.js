9f402349d64c076c9e20d89aacee3bb7bff41b4b
Chanuka999 <chanukaranditha99@gmail.com>
2026-04-11 19:56:31 +0530

    feat:add product image for main file

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 import {
   createProduct,
   getProducts,
@@ -10,13 +11,14 @@ import { authMiddleware } from "../middleware/authMiddleware.js";
 import { requireRole } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.use(authMiddleware, requireRole("admin"));
 
-router.post("/", createProduct);
+router.post("/", upload.single("image"), createProduct);
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-router.put("/:id", updateProduct);
+router.put("/:id", upload.single("image"), updateProduct);
 router.delete("/:id", deleteProduct);
 
 export default router;
