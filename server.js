bac88bf2fc0cd3f009008f59a62233ed68d081f5
Chanuka999 <chanukaranditha99@gmail.com>
2026-04-11 18:41:43 +0530

    feat(routes): add API routes for authentication, users, categories, products, orders, order items, and settings

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import express from "express";
 import sequelize from "./config/database.js";
 import adminRouter from "./admin/admin.js";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import orderItemRoutes from "./routes/orderItemRoutes.js";
+import settingRoutes from "./routes/settingRoutes.js";
+import "./model/index.js";
 
 dotenv.config();
 
@@ -11,7 +19,13 @@ app.use(express.json());
 
 // AdminJS route
 app.use("/admin", adminRouter);
-
+app.use("/api", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/order-items", orderItemRoutes);
+app.use("/api/settings", settingRoutes);
 sequelize.sync({ alter: true }).then(() => {
   app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
